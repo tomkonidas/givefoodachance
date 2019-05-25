3609824ec7d3ad1498fb7b5e9b9970c48a728af2
Tom Konidas <tomkonidas@protonmail.ch>
2019-05-25 13:23:11 -0400

    init

---
+++ src/js/_footer.js
@@ -0,0 +1,5 @@
+// Get the current year for the copyright
+var currentYear = document.querySelector('#currentYear');
+var currentDate = new Date();
+currentYear.textContent=(currentDate.getFullYear());
+
