9df06ba27eeed70673820c22efa3253b75b2fb6c
Tom Konidas <tomkonidas@protonmail.ch>
2019-08-22 21:47:53 -0400

    Cleanups JS

--- src/js/_footer.js
+++
@@ -1,4 +0,0 @@
-// Get the current year for the copyright
-var currentYear = document.querySelector('#currentYear');
-var currentDate = new Date();
-currentYear.textContent=(currentDate.getFullYear());
