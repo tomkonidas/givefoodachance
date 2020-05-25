0117c08ef3bb0973b13008e7b9cc12e0d8debea0
Tom Konidas <tomkonidas@protonmail.ch>
2020-05-25 12:57:21 -0400

    Add netlfiyCMS

--- pages/index.vue
+++ pages/index.vue
@@ -2,40 +2,31 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        givefoodachance
-      </h1>
-      <h2 class="subtitle">
-        The gfac blog
-      </h2>
+      <h1 class="title">givefoodachance</h1>
+      <h2 class="subtitle">The gfac blog</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
+  },
+  head() {
+    return {
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +40,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
