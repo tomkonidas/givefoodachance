81d88d109008288ec30ccf80dc1992a07ec1a46f
Tom Konidas <tomkonidas@protonmail.ch>
2020-05-25 17:07:11 -0400

    Add meta social

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,57 @@ export default {
         content:
           "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
       },
+      // OpenGraph data (Most widely used)
+      { property: "og:title", content: "Give Food a Chance" },
+      { property: "og:site_name", content: "Give Food a Chance" },
+      // The list of types is available here: http://ogp.me/#types
+      { property: "og:type", content: "website" },
+      // Should the the same as your canonical link, see below.
+      {
+        property: "og:url",
+        content: "https://givefoodachance.com",
+      },
+      {
+        property: "og:image",
+        content: "https://givefoodachance.com/og-image.jpg",
+      },
+      // Often the same as your meta description, but not always.
+      {
+        property: "og:description",
+        content:
+          "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
+      },
+
+      // Twitter card
+      { name: "twitter:card", content: "summary" },
+      {
+        name: "twitter:site",
+        content: "https://givefoodachance.com",
+      },
+      { name: "twitter:title", content: "Give Food a Chance" },
+      {
+        name: "twitter:description",
+        content:
+          "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
+      },
+      // Your twitter handle, if you have one.
+      { name: "twitter:creator", content: "@givefoodachance" },
+      {
+        name: "twitter:image:src",
+        content: "https://givefoodachance.com/og-image.jpg",
+      },
+
+      // Google / Schema.org markup:
+      { itemprop: "name", content: "https://givefoodachance.com" },
+      {
+        itemprop: "description",
+        content:
+          "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
+      },
+      {
+        itemprop: "image",
+        content: "https://givefoodachance.com/og-image.jpg",
+      },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
