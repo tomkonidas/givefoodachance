cbab24b85631558edd9f12826101524532764e1e
Tom Konidas <tomkonidas@protonmail.ch>
2020-06-03 12:13:38 -0400

    Clean nuxt from ignore

--- .nuxt/markdown-it.js
+++
@@ -1,9 +0,0 @@
-import MarkdownIt from 'markdown-it'
-
-const handlePlugin = (plugin) => plugin.default || plugin
-
-export default ({ app }, inject) => {
-  const md = new MarkdownIt('default', )
-
-  inject('md', md)
-}
