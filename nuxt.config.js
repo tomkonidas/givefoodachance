55baad4fc5877ed93dfec9459dddbe741aa4f57f
Tom Konidas <tomkonidas@protonmail.ch>
2020-07-06 21:17:00 -0400

    Add content module to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,16 @@
 export default {
-  generate: {
-    routes: function() {
-      const fs = require("fs");
-      const path = require("path");
-      return fs.readdirSync("./assets/content/blog").map((file) => {
-        return {
-          route: `/blog/${path.parse(file).name}`, // Return the slug
-          payload: require(`./assets/content/blog/${file}`),
-        };
-      });
-    },
-  },
+  // generate: {
+  //   routes: function() {
+  //     const fs = require("fs");
+  //     const path = require("path");
+  //     return fs.readdirSync("./assets/content/blog").map((file) => {
+  //       return {
+  //         route: `/blog/${path.parse(file).name}`, // Return the slug
+  //         payload: require(`./assets/content/blog/${file}`),
+  //       };
+  //     });
+  //   },
+  // },
   mode: "universal",
   /*
    ** Auto import components
@@ -109,7 +109,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: ["@nuxtjs/markdownit"],
+  modules: ["@nuxtjs/markdownit", "@nuxt/content"],
   markdownit: {
     injected: true,
   },
