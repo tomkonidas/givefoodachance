5001f53579d7f0e7a8ad6a26052d9afdca4a2f37
Tom Konidas <tomkonidas@protonmail.ch>
2021-02-22 17:27:22 -0500

    Add GoogleAnalytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,87 +1,87 @@
 export default {
-  mode: "universal",
+  mode: 'universal',
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
    */
-  target: "static",
+  target: 'static',
   components: true,
   /*
    ** Headers of the page
    */
   head: {
     htmlAttrs: {
-      lang: "en",
+      lang: 'en',
     },
-    title: "Give Food a Chance",
+    title: 'Give Food a Chance',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { name: "author", content: "Golfo Siozios" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'author', content: 'Golfo Siozios' },
       {
-        hid: "description",
-        name: "description",
+        hid: 'description',
+        name: 'description',
         content:
-          "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
+          '/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.',
       },
       // OpenGraph data (Most widely used)
-      { property: "og:title", content: "Give Food a Chance" },
-      { property: "og:site_name", content: "Give Food a Chance" },
+      { property: 'og:title', content: 'Give Food a Chance' },
+      { property: 'og:site_name', content: 'Give Food a Chance' },
       // The list of types is available here: http://ogp.me/#types
-      { property: "og:type", content: "website" },
+      { property: 'og:type', content: 'website' },
       // Should the the same as your canonical link, see below.
       {
-        property: "og:url",
-        content: "https://givefoodachance.com",
+        property: 'og:url',
+        content: 'https://givefoodachance.com',
       },
       {
-        property: "og:image",
-        content: "https://givefoodachance.com/og-image.jpg",
+        property: 'og:image',
+        content: 'https://givefoodachance.com/og-image.jpg',
       },
       // Often the same as your meta description, but not always.
       {
-        property: "og:description",
+        property: 'og:description',
         content:
-          "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
+          '/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.',
       },
 
       // Twitter card
-      { name: "twitter:card", content: "summary" },
+      { name: 'twitter:card', content: 'summary' },
       {
-        name: "twitter:site",
-        content: "https://givefoodachance.com",
+        name: 'twitter:site',
+        content: 'https://givefoodachance.com',
       },
-      { name: "twitter:title", content: "Give Food a Chance" },
+      { name: 'twitter:title', content: 'Give Food a Chance' },
       {
-        name: "twitter:description",
+        name: 'twitter:description',
         content:
-          "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
+          '/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.',
       },
       // Your twitter handle, if you have one.
-      { name: "twitter:creator", content: "@givefoodachance" },
+      { name: 'twitter:creator', content: '@givefoodachance' },
       {
-        name: "twitter:image",
-        content: "https://givefoodachance.com/og-image.jpg",
+        name: 'twitter:image',
+        content: 'https://givefoodachance.com/og-image.jpg',
       },
 
       // Google / Schema.org markup:
-      { itemprop: "name", content: "Give Food a Chance" },
+      { itemprop: 'name', content: 'Give Food a Chance' },
       {
-        itemprop: "description",
+        itemprop: 'description',
         content:
-          "/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.",
+          '/giv•fōōd•ā•CHans/: Lifestyle allowing food to live up to its full potential by nourishing you & (occasionally) satisfying that sweet tooth.',
       },
       {
-        itemprop: "image",
-        content: "https://givefoodachance.com/og-image.jpg",
+        itemprop: 'image',
+        content: 'https://givefoodachance.com/og-image.jpg',
       },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: "#fff" },
+  loading: { color: '#fff' },
   /*
    ** Global CSS
    */
@@ -89,7 +89,12 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    {
+      src: './plugins/GoogleAnalytics.js',
+      mode: 'client',
+    },
+  ],
   /*
    ** Nuxt.js dev-modules
    */
@@ -97,7 +102,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: ["@nuxtjs/markdownit", "@nuxt/content"],
+  modules: ['@nuxtjs/markdownit', '@nuxt/content'],
   markdownit: {
     injected: true,
   },
---
+++ plugins/GoogleAnalytics.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import VueGtag from 'vue-gtag';
+
+Vue.use(VueGtag, {
+  config: { id: 'G-Z7CLDCWYM6' },
+  appName: 'Give Food A Chance',
+});
